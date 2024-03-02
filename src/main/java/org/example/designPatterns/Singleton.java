72c713d0de17cf80faddc53c9a7a6da78e1263c2
Arthur <arturka0505@gmail.com>
2024-03-02 13:48:36 -0500

    Singleton added

---
+++ src/main/java/org/example/designPatterns/Singleton.java
@@ -0,0 +1,24 @@
+package org.example.designPatterns;
+
+/*
+Singleton Pattern allows us to make sure that the instance
+of class is created and is existed only once for the whole
+program. Singleton also allows us to call object of the class
+from anywhere
+ */
+public class Singleton {
+    private static Singleton singleton;
+    private String value;
+    private Singleton(String value){
+        this.value = value;
+    }
+
+    public static Singleton getInstance(String value){
+        if(singleton == null){
+            singleton = new Singleton(value);
+        }
+        return singleton;
+    }
+
+
+}
